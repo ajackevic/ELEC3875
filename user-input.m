210a04bef1518694f9107100d3c415b1f067311b
ajackevic <el16a2j@leeds.ac.uk>
2019-10-27 17:45:30 +0000

    Update user-input
    I've added alread written script to user-input.m
    
    As of now the script asks the user for input (dialog box) and converts the plaintext to hex values

--- user-input.m
+++ user-input.m
@@ -1,1 +1,26 @@
+% Deleration of veriables
+char user_plaintext;
+hex_plaintext = [];
+hex_str = [];
 
+% setting the dialog box parameters
+dlg_task = "Enter plaintext:";
+dlg_title = "Plaintext dialog";
+dlg_dim = [20 100];
+
+
+user_input = inputdlg(dlg_task, dlg_title, dlg_dim);    % user input from dialog box 
+string_plaintext = num2cell(char(user_input));          % converting cell to char to cell array
+%%disp(string_plaintext);
+
+for i = 1:length(string_plaintext)
+    hex_str = dec2hex(char(string_plaintext(i)));
+    hex_plaintext = [hex_plaintext; hex_str];
+end
+
+% create a function that would take the hex_plaintext as an input
+% add spacebar hex values to the end of the array if the input
+% has a non-zero modulo (use mod function) then seperate the array
+% to 16 bytes
+
+disp(hex_plaintext(1,:));      % displays the first hex value
