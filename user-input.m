f27a1f51c55ecf4c56870175865645e7699fe4cc
ajackevic <el16a2j@leeds.ac.uk>
2019-10-27 20:33:22 +0000

    Matrix 16 hex byte output
    Converted the hex_plaintext long array to a matrix. I've also removed redundunt print statments.

--- user-input.m
+++ user-input.m
@@ -11,32 +11,23 @@ dlg_dim = [20 100];
 
 user_input = inputdlg(dlg_task, dlg_title, dlg_dim);    % user input from dialog box
 string_plaintext = num2cell(char(user_input));          % converting cell to char to cell array
-%%disp(string_plaintext);
 
 for i = 1:length(string_plaintext)
     hex_str = dec2hex(char(string_plaintext(i)));
     hex_plaintext = [hex_plaintext; hex_str];
 end
 
-disp(hex_plaintext(1,:));      % displays the first hex value
-disp(hex_plaintext)
-disp("-------------")
-
-
-%Adds 0x20 (hex space value) to hex_plaintext if value is below 16, to make it
-%a block of 16 bytes
+%Adds 0x20 (hex space value) to hex_plaintext if value if below 16
 if length(hex_plaintext) < 16
     for i = 1:16-length(hex_plaintext)
         hex_plaintext = [hex_plaintext; "20"];
     end
 
-%Adds 0x20 (hex space value) to hex_plaintext if value is above 16, to make is
-%a muiltiple of 16
 elseif mod(length(hex_plaintext),16) ~= 0
-    disp("condition has been met")
     for i = 1:16-mod(length(hex_plaintext),16)
         hex_plaintext = [hex_plaintext; "20"];
     end
 end
 
-disp(hex_plaintext)
+%Convert the hex_plaintext array to a matrix
+AES_block_input = reshape(hex_plaintext,16, length(hex_plaintext)/16)
