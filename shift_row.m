f9de6e464121297480bbc45d29b1af4c1140846f
ajackevic <el16a2j@leeds.ac.uk>
2019-11-01 13:38:40 +0000

    Updated shift_row
    Fixed the second shift operation block (decleartions of temp variable was in the wronge position).
    
    Added the third shift row block and commenting to the sript.

--- shift_row.m
+++ shift_row.m
@@ -1,18 +1,34 @@
-function outputArray = shift_row(inputArray)
-    temp = inputArray(14);
-    inputArray(14) = inputArray(2);
-    inputArray(2) = inputArray(6);
-    inputArray(6) = inputArray(10);
-    inputArray(10) = temp;
+% This function shifts certain rows by a certain amount as required for AES
+function outputBlock = shift_row(inputBlock)
+%{
+    The required shifting operation for AES
+    [  1,  5,  9, 13]    [  1,  5,  9, 13]
+    [  2,  6, 10, 14] => [  6, 10, 14,  2]
+    [  3,  7, 11, 15] => [ 11, 15,  3,  7]
+    [  4,  8, 12, 16]    [ 16,  4,  8, 12]
+%}
 
+    % Shift second row to the left by one
+    temp1 = inputBlock(14);
+    inputBlock(14) = inputBlock(2);
+    inputBlock(2) = inputBlock(6);
+    inputBlock(6) = inputBlock(10);
+    inputBlock(10) = temp1;
 
-    inputArray(3) = inputArray(11);
-    inputArray(11) = temp;
-    inputArray(7) = inputArray(15);
-    temp = inputArray(3);
-    inputArray(11) = temp;
-    temp = inputArray(7);
-    inputArray(15) = temp;
+    % Shift third row to the left by one
+    temp1 = inputBlock(7);
+    temp2 = inputBlock(3);
+    inputBlock(3) = inputBlock(11);
+    inputBlock(7) = inputBlock(15);
+    inputBlock(15) = temp1;
+    inputBlock(11) = temp2;
 
-    disp(inputArray)
+    % Shift fourth row to the left by one
+    temp1 = inputBlock(8);
+    inputBlock(8) = inputBlock(4);
+    inputBlock(4) = inputBlock(16);
+    inputBlock(16) = inputBlock(12);
+    inputBlock(12) = temp1;
+
+    outputBlock = inputBlock;
 end
