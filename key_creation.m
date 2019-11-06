e22f27ae821109620430899ea6f0885c0b02aa11
ajackevic <el16a2j@leeds.ac.uk>
2019-11-06 13:16:38 +0000

    Updated key_creation
    I've added code which as of now:
    1) Takes the last four key values and stores it in an array
    2) Shifts these values to the left by one and then subsitutes these values for s-box values
    3)Takes the values of key three columns down and sotres it in an array
    4) XOR's the two arrays

--- key_creation.m
+++ key_creation.m
@@ -1,10 +1,20 @@
 function roundKeys = key_creation()
+
     AESKey = ["54"; "68"; "61"; "74"; "73"; "20"; "6D"; "79"; "20"; "4B"; "75"; "6E"; "67"; "20"; "46"; "75"];
     Rcon = ["01"; "02"; "04"; "08"; "10"; "20"; "40"; "80"; "1B"; "36"];
+    allKeys = AESKey;
 
+    for column = 0:3
+        % This gives the last four values of allKeys
+        lastColumn = allKeys(end-3:end);
+        shiftSub = [s_box(lastColumn(2)); s_box(lastColumn(3)); s_box(lastColumn(4)); s_box(lastColumn(1))];
+        threePosDown = allKeys(end-15:end-12);
 
-    for i = 0:3
-        temp_key = [AESKey((i*4)+1); AESKey((i*4)+2); AESKey((i*4)+3); AESKey((i*4)+4)];
-        disp(temp_key)
+        output = [...
+                 string(dec2hex(bitxor(hex2dec(threePosDown(1)), hex2dec(shiftSub(1))),2));...
+                 string(dec2hex(bitxor(hex2dec(threePosDown(2)), hex2dec(shiftSub(2))),2));...
+                 string(dec2hex(bitxor(hex2dec(threePosDown(3)), hex2dec(shiftSub(3))),2));...
+                 string(dec2hex(bitxor(hex2dec(threePosDown(4)), hex2dec(shiftSub(4))),2));...
+                 ];
     end
 end
