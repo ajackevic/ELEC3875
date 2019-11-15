6dea0593f69380c8e59fb1db3ac4740e1e8186b3
ajackevic <el16a2j@leeds.ac.uk>
2019-11-15 11:54:30 +0000

    10th order enchancement ID:07
    Added a while loop which checks the order of the polynomial and repeatedly does the modulo operation with 0x11B until the polynomial is reduced to an 8-bit value.

--- Encryption/gf_mul.m
+++ Encryption/gf_mul.m
@@ -1,22 +1,31 @@
-% This function calculates multiplication in gf(2^8) field,
-% with modulo 0x11B for overflow of 8 bits.
-% Note: output is a decimal value not hex.
+% This function calculates multiplication in gf(2^8) field, with modulo 0x11B for any 8 bit overflow
+% Note: output is a decimal value not hex
 function value_out = gf_mul(value1_in, value2_in)
 
-    % Converting hex to decimal to binary
+    % Converting hex to decimal to binary for gfconv()
     value1_in = de2bi(hex2dec(value1_in));
     value2_in = de2bi(hex2dec(value2_in));
-
     % Multiplication of polynomials in GF order 2.
     gf_muil_b = gfconv(value1_in, value2_in,2);
+    gf_muil_d = bi2de(gf_muil_b);
 
-    % Modulos the multipied value with 0x11B(100011011) if value is a 9 bit value.
-    if length(gf_muil_b) > 8
-        % 0x11B(100011011) is reversed partially as bi2de requires LSB to be LHS and
-        % due to gfconv outputting in this format.
-        gf_muil_b = bitxor(gf_muil_b, [1 1 0 1 1 0 0 0 1]);
+    % Whilst gf_muil_d is above GF(2^8)(overflow), XOR with 0x11B starting from MSB
+    % Note: decimal values are used instead of binary mainly due to bitxor requiring the array size to
+    % be identical (between gf_muil and 0x11b) and the fact that once XOR'd MSB will still be 0 instead
+    % of reducing array size automatically. Hence another function would have to find an MSB that is a 1.
+    while gf_muil_d >= 256
+        % If gf_muil_d is of 10th order, XOR with [1 0 0 0 1 1 0 1 1 0 0](1132)
+        if gf_muil_d >= 1024 % 10th order
+            gf_muil_d = bitxor(gf_muil_d, 1132);
+        end
+        % If gf_muil_d is of 9th order, XOR with [1 0 0 0 1 1 0 1 1 0](566)
+        if gf_muil_d >= 512 & gf_muil_d <= 1023
+            gf_muil_d = bitxor(gf_muil_d, 566);
+        end
+        % If gf_muil_d is of 8th order, XOR with [1 0 0 0 1 1 0 1 1 ](283)
+        if gf_muil_d >= 256 & gf_muil_d <= 511
+            gf_muil_d = bitxor(gf_muil_d, 283);
+        end
     end
-
-    % Converting binary to decimal
-    value_out = bi2de(gf_muil_b);
+    value_out = gf_muil_d;
 end
