d166bee086beafc804e7499abc9005e07fb5f215
ajackevic <el16a2j@leeds.ac.uk>
2019-11-17 18:03:33 +0000

    Combined sub_byte
    I've combined the two sub_byte script. The function requires a new argument called "type" which could either be "encrypt" or "decrypt".

--- sub_byte.m
+++ sub_byte.m
@@ -1,9 +1,9 @@
 % This function substitutes an input 16-byte string array with its corresponding s-box values
-function sBoxOutput = sub_byte(sBoxInput)
+function sBoxOutput = sub_byte(sBoxInput, type)
     sBoxOutput = [];
     for i = 1:length(sBoxInput)
         % S-box substituting by proving the value to the s_box function
-        sBoxValue = s_box(sBoxInput(i));
+        sBoxValue = s_box(sBoxInput(i), type);
         % Adding the variable sBoxValue to the sBoxOutput array
         sBoxOutput = [sBoxOutput ; sBoxValue];
     end
