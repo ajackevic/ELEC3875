578c404e74c7e6cb861164f4e0126e3308939dd9
ajackevic <el16a2j@leeds.ac.uk>
2019-11-21 16:58:13 +0000

    Un-doing previous commit
    This commit is to remove previous commits work. That commit should have been in a new created branch.

--- Sub scripts/key_creation.m
+++ Sub scripts/key_creation.m
@@ -1,57 +1,44 @@
-% This function creates the required amount of key for the selected AES mode
-function roundKeys = key_creation(inputKey, keyType, AESMode)
-    roundKeys = [];
+% This function creates 10 different round keys from the supplied original key
+function roundKeys = key_creation(inputKey, keyType)
+
     AESKey = key_format(inputKey, keyType);
     Rcon = ["01"; "02"; "04"; "08"; "10"; "20"; "40"; "80"; "1B"; "36"];
-    % Declaration of different variables for 128 or 196 bit AES
-    if AESMode == "128-bit"
-        roundsLimit = 11; columnLimit = 4; blockLimit = 44; iLimit = 10;
-    end
-    if  AESMode == "192-bit"
-        roundsLimit = 13; columnLimit = 6; blockLimit = 52; iLimit = 8;
-    end
-    % Create a 4x4(128-bit)/4x6(192-bit) matrix
-    allKeys = reshape(AESKey,4,columnLimit);
-    for i = 1:iLimit
-        for column = 1:columnLimit
-            % Shift the values to the left by one and s-box substitution if this is the first column
-            % of the key/round key
-            if column == 1
-                shiftSub = [ ...
-                          s_box(allKeys(2,end), "encrypt"); s_box(allKeys(3,end), "encrypt"); ...
-                          s_box(allKeys(4,end), "encrypt"); s_box(allKeys(1,end), "encrypt"); ...
-                          ];
-                newColumn = [...
-                          bitxor(hex2dec(allKeys(1,end-(columnLimit-1))), hex2dec(shiftSub(1))); ...
-                          bitxor(hex2dec(allKeys(2,end-(columnLimit-1))), hex2dec(shiftSub(2))); ...
-                          bitxor(hex2dec(allKeys(3,end-(columnLimit-1))), hex2dec(shiftSub(3))); ...
-                          bitxor(hex2dec(allKeys(4,end-(columnLimit-1))), hex2dec(shiftSub(4))); ...
-                          ];
+    allKeys = AESKey;
 
-                % XOR first value of newColumn with corresponding Rcon value
-                newColumn(1) = bitxor(newColumn(1),hex2dec(Rcon(i)));
-            else
-                % XOR operation between three possitions down (128-bit) / five possition down (192-bit)
-                % column and last current last column
+    for rounds = 1:10
+        for column = 0:3
+
+            % Acquire the last four values (W[0]) and the column thats three positions further back (W[3])
+            lastColumn = allKeys(end-3:end);
+            threePosDown = hex2dec(allKeys(end-15:end-12));
+
+            if column == 0
+                % Do the following if this is the first column of the key
+                % Shift the values to the left by one and s-box substitution
+                shiftSub = hex2dec([ ...
+                                    s_box(lastColumn(2), "encrypt"); s_box(lastColumn(3), "encrypt"); ...
+                                    s_box(lastColumn(4), "encrypt"); s_box(lastColumn(1), "encrypt"); ...
+                                   ]);
                 newColumn = [...
-                          bitxor(hex2dec(allKeys(1,end-(columnLimit-1))), hex2dec(allKeys(1,end))); ...
-                          bitxor(hex2dec(allKeys(2,end-(columnLimit-1))), hex2dec(allKeys(2,end))); ...
-                          bitxor(hex2dec(allKeys(3,end-(columnLimit-1))), hex2dec(allKeys(3,end))); ...
-                          bitxor(hex2dec(allKeys(4,end-(columnLimit-1))), hex2dec(allKeys(4,end)));
-                          ];
+                          bitxor(threePosDown(1), shiftSub(1)); bitxor(threePosDown(2), shiftSub(2)); ...
+                          bitxor(threePosDown(3), shiftSub(3)); bitxor(threePosDown(4), shiftSub(4)); ...
+                         ];
+
+                % XOR first value of newColumn with Rcon
+                newColumn(1) = bitxor(newColumn(1),hex2dec(Rcon(rounds)));
+
+            else
+                % Convert hex to dec for the XOR operation between threePosDown array and lastColumn array
+                lastColumn = hex2dec(lastColumn);
+                newColumn = [...
+                            bitxor(threePosDown(1), lastColumn(1)); bitxor(threePosDown(2), lastColumn(2)); ...
+                            bitxor(threePosDown(3), lastColumn(3)); bitxor(threePosDown(4), lastColumn(4)); ...
+                            ];
             end
-            % Add the new column to the allKey matrix
-            newColumnKey = string(dec2hex(newColumn,2));
-            allKeys = [allKeys reshape(newColumnKey,4,1)];
+            columnKey = string(dec2hex(newColumn,2));
+            allKeys = [allKeys; columnKey];
         end
     end
-    % Delete any unnecessary keys. 192-bit creates 54 column keys but only needs 52
-    blockKey = allKeys(:,1:blockLimit);
-    for rounds = 1:roundsLimit
-        % Reshape the 44(128-bit)/52(192-bit) column of keys to 16x11(128-bits)/16x13(192-bits)
-        roundKeys = [roundKeys reshape(blockKey(1:16),16,1)];
-        if length(blockKey) > 4
-            blockKey = blockKey(:,5:end);
-        end
-    end
+    % Convert the allKeys array to into a readable and usable format
+    roundKeys = reshape(allKeys,16, length(allKeys)/16);
 end
