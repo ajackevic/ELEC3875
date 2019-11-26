203895bde25ad7c69f56749bdadb97f087a7e950
ajackevic <el16a2j@leeds.ac.uk>
2019-11-26 10:52:39 +0000

    Full 256-bit AES implementation
    I've changed other parts of the scripts to allow the use of the 256-bit key. This required small changes in main_encrypt, main_decrypt, key_check and GUI.

--- Sub scripts/key_check.m
+++ Sub scripts/key_check.m
@@ -1,12 +1,24 @@
 % This function checks the whether the input key is a valid input
-function keyFlag = key_check(inputKey, keyType)
+function keyFlag = key_check(inputKey, keyType, AESMode)
     inputKey = char(inputKey);
     % Set flag to false, if criteria is not met set flag to true
+    if AESMode == "128-bit"
+        hexKeyLength = 32;
+        plaintextKeyLength = 16;
+    end
+    if AESMode == "192-bit"
+        hexKeyLength = 48;
+        plaintextKeyLength = 24;
+    end
+    if AESMode == "256-bit"
+        hexKeyLength = 64;
+        plaintextKeyLength = 32;
+    end
     keyFlag = false;
     if keyType == "Hex"
         hexValues = ["0"; "1"; "2"; "3"; "4"; "5"; "6"; "7"; "8"; "9"; "a"; "b"; "c"; "d"; "e"; "f"; "A"; "B"; "C"; "D"; "E"; "F"];
-        % Check length of key is 32 bits
-        if length(inputKey) ~= 32
+        % Check length of key is 32/48 bits
+        if length(inputKey) ~= hexKeyLength
             keyFlag = true;
         end
         % Check if every character in key is a valid hex value
@@ -17,8 +29,8 @@ function keyFlag = key_check(inputKey, keyType)
         end
     end
     if keyType == "Plaintext"
-        % Check if key length is a 16 byte value
-        if length(inputKey) ~= 16
+        % Check if key length is a 16/24 byte value
+        if length(inputKey) ~= plaintextKeyLength
             keyFlag = true;
         end
     end
