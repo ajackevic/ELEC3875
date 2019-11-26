caf0f36e195a20b6506895b3fcc95ccbf15b7dd0
ajackevic <el16a2j@leeds.ac.uk>
2019-11-26 10:46:50 +0000

    key_creation 256-bit
    I've implemented the key creation for the 256-bit key in key_creation. This was relatively easy with the existing infrastructure, except it required another operation when the column is equal to 4 (XOR(W[i-1],w[i-Nk]), where for 256-bit Nk=8 and i = [1,2,3...64].

--- Sub scripts/key_creation.m
+++ Sub scripts/key_creation.m
@@ -1,44 +1,74 @@
-% This function creates 10 different round keys from the supplied original key
-function roundKeys = key_creation(inputKey, keyType)
-
+% This function creates the required amount of key for the selected AES mode
+function roundKeys = key_creation(inputKey, keyType, AESMode)
+    roundKeys = [];
     AESKey = key_format(inputKey, keyType);
     Rcon = ["01"; "02"; "04"; "08"; "10"; "20"; "40"; "80"; "1B"; "36"];
-    allKeys = AESKey;
-
-    for rounds = 1:10
-        for column = 0:3
-
-            % Acquire the last four values (W[0]) and the column thats three positions further back (W[3])
-            lastColumn = allKeys(end-3:end);
-            threePosDown = hex2dec(allKeys(end-15:end-12));
-
-            if column == 0
-                % Do the following if this is the first column of the key
-                % Shift the values to the left by one and s-box substitution
-                shiftSub = hex2dec([ ...
-                                    s_box(lastColumn(2), "encrypt"); s_box(lastColumn(3), "encrypt"); ...
-                                    s_box(lastColumn(4), "encrypt"); s_box(lastColumn(1), "encrypt"); ...
-                                   ]);
+    % Declaration of different variables for 128 or 196 bit AES
+    if AESMode == "128-bit"
+        roundsLimit = 11; columnLimit = 4; blockLimit = 44; iLimit = 10;
+    end
+    if  AESMode == "192-bit"
+        roundsLimit = 13; columnLimit = 6; blockLimit = 52; iLimit = 8;
+    end
+    if AESMode == "256-bit"
+        roundsLimit = 15; columnLimit = 8; blockLimit = 60; iLimit = 7;
+    end
+    % Create a 4x4(128-bit)/4x6(192-bit) matrix
+    allKeys = reshape(AESKey,4,columnLimit);
+    for i = 1:iLimit
+        for column = 1:columnLimit
+            % Shift the values to the left by one and s-box substitution if this is the first column
+            % of the key/round key
+            if column == 1
+                shiftSub = [...
+                          s_box(allKeys(2,end), "encrypt"); s_box(allKeys(3,end), "encrypt"); ...
+                          s_box(allKeys(4,end), "encrypt"); s_box(allKeys(1,end), "encrypt"); ...
+                          ];
                 newColumn = [...
-                          bitxor(threePosDown(1), shiftSub(1)); bitxor(threePosDown(2), shiftSub(2)); ...
-                          bitxor(threePosDown(3), shiftSub(3)); bitxor(threePosDown(4), shiftSub(4)); ...
-                         ];
+                          bitxor(hex2dec(allKeys(1,end-(columnLimit-1))), hex2dec(shiftSub(1))); ...
+                          bitxor(hex2dec(allKeys(2,end-(columnLimit-1))), hex2dec(shiftSub(2))); ...
+                          bitxor(hex2dec(allKeys(3,end-(columnLimit-1))), hex2dec(shiftSub(3))); ...
+                          bitxor(hex2dec(allKeys(4,end-(columnLimit-1))), hex2dec(shiftSub(4))); ...
+                          ];
 
-                % XOR first value of newColumn with Rcon
-                newColumn(1) = bitxor(newColumn(1),hex2dec(Rcon(rounds)));
-
+                % XOR first value of newColumn with corresponding Rcon value
+                newColumn(1) = bitxor(newColumn(1),hex2dec(Rcon(i)));
+            elseif column == 5 && AESMode == "256-bit"
+                % For 256-bit, XOR s-box(last column) and column 8 possitions down (7 as we include last colmn as 1).
+                % Only do this when column is equal to 4 (5 as we start from 1)
+                subColumn = [...
+                          s_box(allKeys(1,end), "encrypt"); s_box(allKeys(2,end), "encrypt"); ...
+                          s_box(allKeys(3,end), "encrypt"); s_box(allKeys(4,end), "encrypt"); ...
+                          ];
+                newColumn = [...
+                          bitxor(hex2dec(allKeys(1,end-(columnLimit-1))), hex2dec(subColumn(1))); ...
+                          bitxor(hex2dec(allKeys(2,end-(columnLimit-1))), hex2dec(subColumn(2))); ...
+                          bitxor(hex2dec(allKeys(3,end-(columnLimit-1))), hex2dec(subColumn(3))); ...
+                          bitxor(hex2dec(allKeys(4,end-(columnLimit-1))), hex2dec(subColumn(4))); ...
+                          ];
             else
-                % Convert hex to dec for the XOR operation between threePosDown array and lastColumn array
-                lastColumn = hex2dec(lastColumn);
+                % XOR operation between 3pos down(128-bit)/5pos down (192-bit)/7pos down (256-bit)
+                % column and current last column
                 newColumn = [...
-                            bitxor(threePosDown(1), lastColumn(1)); bitxor(threePosDown(2), lastColumn(2)); ...
-                            bitxor(threePosDown(3), lastColumn(3)); bitxor(threePosDown(4), lastColumn(4)); ...
-                            ];
+                          bitxor(hex2dec(allKeys(1,end-(columnLimit-1))), hex2dec(allKeys(1,end))); ...
+                          bitxor(hex2dec(allKeys(2,end-(columnLimit-1))), hex2dec(allKeys(2,end))); ...
+                          bitxor(hex2dec(allKeys(3,end-(columnLimit-1))), hex2dec(allKeys(3,end))); ...
+                          bitxor(hex2dec(allKeys(4,end-(columnLimit-1))), hex2dec(allKeys(4,end)));
+                          ];
             end
-            columnKey = string(dec2hex(newColumn,2));
-            allKeys = [allKeys; columnKey];
+            % Add the new column to the allKey matrix
+            newColumnKey = string(dec2hex(newColumn,2));
+            allKeys = [allKeys reshape(newColumnKey,4,1)];
         end
     end
-    % Convert the allKeys array to into a readable and usable format
-    roundKeys = reshape(allKeys,16, length(allKeys)/16);
+    % Delete any unnecessary keys. 192-bit creates 54 column keys but only needs
+    % 52. 256-bit creates 64 but only requires 60.
+    blockKey = allKeys(:,1:blockLimit);
+    for rounds = 1:roundsLimit
+        % Reshape the 44(128-bit)/52(192-bit)/60(256-bit) column of keys to 16x11(128-bit)/16x13(192-bit)/16x15(256-bit)
+        roundKeys = [roundKeys reshape(blockKey(1:16),16,1)];
+        if length(blockKey) > 4
+            blockKey = blockKey(:,5:end);
+        end
+    end
 end
