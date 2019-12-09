5f065a8995fe1c112fcdeed71697e89e64572f52
ajackevic <el16a2j@leeds.ac.uk>
2019-12-09 22:05:38 +0000

    Updated formate encrypt script
    I've fixed the two issues (ID:16 & ID:18), this was a relativly easy fix as all it involved was removing the for statment (as that didn't do much) and add an else statment in the if-elseif statment.

--- Sub scripts/format_encrypt_in.m
+++ Sub scripts/format_encrypt_in.m
@@ -1,13 +1,10 @@
 % This function acquires user input and format it into hex 16 byte blocks
-function hexBlockOutput = format_encrypt_in(userData)
+function hexBlockOutput = format_encrypt_in(userData, dataFormat)
     hexPlaintext = [];
     hexChar = [];
 
-    % Converts ASCII to hex and stored in hexPlaintext array
-    for i = 1:length(userData)
-        hexChar = dec2hex(char(userData(i)));
-        hexPlaintext = [hexPlaintext; hexChar];
-    end
+    % Converts ASCII to hex and stored in hexPlaintext trray
+    hexPlaintext = string(dec2hex(char(userData)));
 
     % Adds 0x20 (hex space value) to hexPlaintext if array value is not a muilptiple of 16
     if length(hexPlaintext) <= 16
@@ -21,6 +18,8 @@ function hexBlockOutput = format_encrypt_in(userData)
         end
         % Convert the hexPlaintext array to a matrix
         hexBlockOutput = reshape(hexPlaintext,16, length(hexPlaintext)/16);
+    else
+        hexBlockOutput = reshape(string(hexPlaintext),16, length(hexPlaintext)/16);
     end
     hexBlockOutput = string(hexBlockOutput);
 end
