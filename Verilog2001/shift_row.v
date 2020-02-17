95822241fc09fa1da17d31fc54b99f9a10218653
ajackevic <el16a2j@leeds.ac.uk>
2020-02-17 16:44:38 +0000

    Data flip of shift_row
    Seems the data output of shift_row was inverted thus I changed it so that the data is read left ro right. Simillar issue as key_creation

--- Verilog2001/shift_row.v
+++ Verilog2001/shift_row.v
@@ -4,13 +4,15 @@ module shift_row(
 );
 
 	assign outputData = {inputData[127:120], inputData[87:80],
-								inputData[47:40],   inputData[7:0],
-								inputData[95:88],	  inputData[55:48],
-								inputData[15:8],	  inputData[103:96],
-								inputData[63:56],   inputData[23:16],
-								inputData[111:104], inputData[71:64],
-								inputData[31:24],   inputData[119:112],
-								inputData[79:72],   inputData[39:32]
+							   inputData[47:40],   inputData[7:0],
+							   inputData[95:88],	 inputData[55:48],
+							   inputData[15:8],	   inputData[103:96],
+							   inputData[63:56],   inputData[23:16],
+							   inputData[111:104], inputData[71:64],
+							   inputData[31:24],   inputData[119:112],
+							   inputData[79:72],	 inputData[39:32],
+								inputData[127:120],  inputData[87:80],
+								inputData[47:40],    inputData[7:0]
 							  };
 
 endmodule
