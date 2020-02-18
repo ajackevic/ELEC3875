725644d89c965603f1696653816f4cc0882c7358
ajackevic <el16a2j@leeds.ac.uk>
2020-02-18 17:11:43 +0000

    add_round_key_tb Improvment
    I've changed the test bench of add_round_key. I've removed the use of $monitor and instead replaced it with an if statment between a comparision of the expected value.

--- Verilog2001/TestBenches/add_round_key_tb.v
+++ Verilog2001/TestBenches/add_round_key_tb.v
@@ -1,25 +1,47 @@
-`timescale 1ns/100ps
 module add_round_key_tb;
 
 reg  [127:0] roundKey;
-reg  [127:0] roundData;
-wire [127:0] addRoundKeyOutput;
+reg  [127:0] inputData;
+reg  [127:0] expectedValue;
+wire [127:0] outputData;
 
 add_round_key dut (
-	.data						(roundData),
-	.roundKey				(roundKey),
-	.addRoundKeyOutput	(addRoundKeyOutput)
+	.inputData	(inputData),
+	.roundKey	(roundKey),
+	.outputData	(outputData)
 );
 
 initial begin
-	$monitor("RoundData: %h \n", roundData,
-				"RoundKey: %h \n", roundKey,
-				"AddRoundKeyOutput: %h \n", addRoundKeyOutput,
-			  );
-	roundKey = 128'h5468617473206D79204B756E67204675;
-	roundData = 128'h54776F204F6E65204E696E652054776F;
-	#10;
+	expectedValue = 128'h1a3174470b1b226e59084e3c540e1f00;
+end
+
+
+always @ (*) begin
+
+	roundKey = 128'h754620676e754b20796d207374616854;
+	inputData = 128'h6f775420656e694e20656e4f206f7754;
+
+	if (outputData != expectedValue) begin
+		$display("Fail \n \n",
+					"For the following inputs: \n",
+					"Input Key: %h \n", roundKey,
+					"Input data: %h \n \n", inputData,
+					"Expected output: \n",
+					"Output data: %h \n \n", expectedValue,
+					"Aquired output: \n",
+					"Output data: %h \n", outputData
+				  );
+	end
+
+	if (outputData == expectedValue) begin
+		$display("Pass \n \n",
+					"For the following inputs: \n",
+					"Input Key: %h \n", roundKey,
+					"Input data: %h \n \n", inputData,
+					"Aquired output: \n",
+					"Output data: %h \n \n", outputData
+				  );
+	end
+
 end
 endmodule
-
-	
