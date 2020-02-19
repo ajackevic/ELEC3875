0fc4387cbe62ab59786ad20efe372c11545ea6a3
ajackevic <el16a2j@leeds.ac.uk>
2020-02-19 11:10:47 +0000

    s_box_tb upgrade
    I've changed s_box_tb to a more appropriate test bench

--- Verilog2001/TestBenches/s_box_tb.v
+++ Verilog2001/TestBenches/s_box_tb.v
@@ -1,37 +1,63 @@
-`timescale 1ns/100ps
 module s_box_tb;
 
-reg [7:0] inputValue;
-wire [7:0] outputValue;
+reg  [7:0] inputValue1;
+reg  [7:0] inputValue2;
+reg  [7:0] inputValue3;
+reg  [7:0] expectedValue1;
+reg  [7:0] expectedValue2;
+reg  [7:0] expectedValue3;
+wire [7:0] outputValue1;
+wire [7:0] outputValue2;
+wire [7:0] outputValue3;
 
-s_box dut (
-	.sboxInput		(inputValue),
-	.sboxOutput	(outputValue)
+s_box dut1(
+	.sboxInput	(inputValue1),
+	.sboxOutput	(outputValue1)
 );
 
+s_box dut2(
+	.sboxInput	(inputValue2),
+	.sboxOutput	(outputValue2)
+);
+
+s_box dut3(
+	.sboxInput	(inputValue3),
+	.sboxOutput	(outputValue3)
+);
+
+
 initial begin
-	
-	$display("Time start: %d ns", $time);
-	$monitor ("Time: %d ns\ Input: %h \ Output: %h", $time, inputValue, outputValue);
-	inputValue = 8'h12;
-	#10;
-	inputValue = 8'hAD;
-	#10;
-	inputValue = 8'hA0;
-	#10;
-	inputValue = 8'h5F;
-	#10;
-	inputValue = 8'h9A;
-	#10;
-	inputValue = 8'h34;
-	#10;
-	inputValue = 8'h00;
-	#10;
-	inputValue = 8'h73;
-	#10;
-	inputValue = 8'hFF;
-	#10;
-	$display("Time end: %d ns", $time);
+	expectedValue1 = 8'h90;
+	expectedValue2 = 8'h5c;
+	expectedValue3 = 8'h76;
+end
+
+
+always @(*) begin
+	inputValue1 = 8'h96;
+	inputValue2 = 8'ha7;
+	inputValue3 = 8'h0f;
+
+	if((outputValue1 != expectedValue1) | (outputValue2 != expectedValue2) | (outputValue3 != expectedValue3))begin
+		$display("Fail \n \n",
+					"For the following inputs: \n",
+					"Input Value: %h, %h, %h \n \n", inputValue1, inputValue2, inputValue3,
+					"Expected output: \n",
+					"Output Value: %h, %h, %h \n \n", expectedValue1, expectedValue2, expectedValue3,
+					"Aquired output: \n",
+					"Output Value: %h, %h, %h \n", outputValue1, outputValue2, outputValue3
+				  );
+	end
+
+	if((outputValue1 == expectedValue1) & (outputValue2 == expectedValue2) & (outputValue3 == expectedValue3))begin
+		$display("Pass \n \n",
+					"For the following inputs: \n",
+					"Input Value: %h, %h, %h \n \n", inputValue1, inputValue2, inputValue3,
+					"Aquired output: \n",
+					"Output Value: %h, %h, %h \n", outputValue1, outputValue2, outputValue3
+				  );
+	end
+
 end
 endmodule
 	
